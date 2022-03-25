5bb624927497097492985e0a5450da9efc3fc4c6
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-24 21:15:16 -0300

    create is odd or even

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi there!")
+	n := createSlice()
+	checkOddsAndEvens(n)
+}
+
+func createSlice() []int {
+	var n []int
+	for i := 0; i <= 10; i++ {
+		n = append(n, i)
+	}
+	return n
+}
+
+func checkOddsAndEvens(n []int) {
+	for _, v := range n {
+		if v % 2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		} 
+	}
 }
